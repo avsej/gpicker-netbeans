0be3ead6ac68b02def21815c3c20a4a1d0983922
Aliaksey Kandratsenka <alk@tut.by>
2010-06-30 12:57:35 +0300

    run gpicker from SwingWorker

--- src/org/gpicker/actions/GPickFile.java
+++ src/org/gpicker/actions/GPickFile.java
@@ -18,9 +18,10 @@ package org.gpicker.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.prefs.Preferences;
+import javax.swing.SwingWorker;
 import org.netbeans.api.project.FileOwnerQuery;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ui.OpenProjects;
@@ -32,36 +33,61 @@ import org.openide.util.NbPreferences;
 
 public final class GPickFile implements ActionListener {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        Preferences pref = NbPreferences.forModule(GPickFile.class);
-        String executable = pref.get("gpicker_executable", "gpicker").trim();
-        String path = pref.get("gpicker_path", System.getenv("PATH")).trim();
+    private Project getProject() {
+        FileObject file = EditorContextDispatcher.getDefault().getCurrentFile();
+        Project project;
+        if (file != null) {
+            project = FileOwnerQuery.getOwner(file);
+        } else {
+            OpenProjects op = OpenProjects.getDefault();
+            project = op.getMainProject();
+            if (project == null && op.getOpenProjects().length > 0) {
+                project = op.getOpenProjects()[0];
+            }
+        }
+        return project;
+    }
+    private class Opener extends SwingWorker {
+        private final String path;
+        private final Project project;
+        private final String executable;
+        private final ArrayList<String> result = new ArrayList<String>();
 
-        try {
-            FileObject file = EditorContextDispatcher.getDefault().getCurrentFile();
-            Project project;
-            if (file != null) {
-                project = FileOwnerQuery.getOwner(file);
-            } else {
-                OpenProjects op = OpenProjects.getDefault();
-                project = op.getMainProject();
-                if (project == null && op.getOpenProjects().length > 0) {
-                    project = op.getOpenProjects()[0];
-                }
+        public Opener(Project project) {
+            Preferences pref = NbPreferences.forModule(GPickFile.class);
+            this.executable = pref.get("gpicker_executable", "gpicker").trim();
+            this.path = pref.get("gpicker_path", System.getenv("PATH")).trim();
+
+            this.project = project;
+        }
+
+        @Override
+        protected Object doInBackground() throws Exception {
+            ProcessBuilder pb = new ProcessBuilder(executable,
+                        "-t", "guess",
+                        project.getProjectDirectory().getPath());
+            pb.environment().put("PATH", path);
+            Process p = pb.start();
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                result.add(s);
+            }
+            if ((s = stdError.readLine()) != null) {
+                System.err.println("[gpicker] Error while execution gpicker: " + s + "\n\tPATH: " + path);
             }
 
-            if (project != null) {
-                ProcessBuilder pb = new ProcessBuilder(executable, "-t", "guess", project.getProjectDirectory().getPath());
-                pb.environment().put("PATH", path);
-                Process p = pb.start();
+            return null;
+        }
 
-                BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-                String s = null;
-                while ((s = stdInput.readLine()) != null) {
-                    file = project.getProjectDirectory().getFileObject(s);
+        @Override
+        protected void done() {
+            try {
+                for (String s : result) {
+                    FileObject file = project.getProjectDirectory().getFileObject(s);
                     if (file != null) {
                         OpenCookie open = DataObject.find(file).getCookie(OpenCookie.class);
                         if (open != null) {
@@ -73,14 +99,20 @@ public final class GPickFile implements ActionListener {
                         System.err.println("[gpicker] Cannot find file: " + s);
                     }
                 }
-                if ((s = stdError.readLine()) != null) {
-                    System.err.println("[gpicker] Error while execution gpicker: " + s + "\n\tPATH: " + path);
-                }
-            } else {
-                System.err.println("[gpicker] Project not found. Nothing to pick.");
+            } catch (Exception e) {
+                System.err.println("Ignoring exception from worker's done " + e.getMessage());
+                e.printStackTrace();
             }
-        } catch (IOException ex) {
-            System.out.println("[gpicker] Cannot execute gpicker: " + ex.getMessage() + "\n\tPATH: " + path);
         }
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Project project = getProject();
+        if (project == null) {
+            System.err.println("[gpicker] Project not found. Nothing to pick.");
+            return;
+        }
+        (new Opener(project)).execute();
     }
 }
